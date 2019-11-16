5484aa4193bcac615d611904a25f82f4474778c4
Anna Carey <anna@annajcarey.com>
2019-11-16 16:33:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
     values.each do |category, names|
       names.each do |name|
       if new_hash.key?(name)
-        new_hash[ = categoriesname]
+        new_hash[categories] = category
       else 
         new_hash[name] = {}
       end 
