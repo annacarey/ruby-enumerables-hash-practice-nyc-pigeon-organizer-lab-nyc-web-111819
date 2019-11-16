753a79414407f04edbd064aba8e310f2fd3f2691
Anna Carey <anna@annajcarey.com>
2019-11-16 16:51:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,13 +8,14 @@ def nyc_pigeon_organizer(data)
       if new_hash.key?(name) == false
         new_hash[name] = {}
       end 
-    #if new_hash[name].key?(category)
-    ##  else 
-    #    new_hash[name][categories] << category
-    #  end 
-    # new_hash[name][categories] = category
   end 
 end
 pp new_hash
 end 
 end 
+
+#if new_hash[name].key?(category)
+    ##  else 
+    #    new_hash[name][categories] << category
+    #  end 
+    # new_hash[name][categories] = category
