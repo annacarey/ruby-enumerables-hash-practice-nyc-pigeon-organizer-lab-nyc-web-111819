277ae90dcece7b9e99b6bbf6ebc7a899f36bb899
Anna Carey <anna@annajcarey.com>
2019-11-16 15:59:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.reduce ({}) do |new_pigeons, (key, value)|
+    value.each do |color, names|
+      if new_pigeons.key?(names)
+        key = names
+      end 
+    end 
+    p value
+  end 
 end
