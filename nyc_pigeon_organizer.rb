9cdc0b139ef3d78f87cc94b919ac3aade0fdfc46
Anna Carey <anna@annajcarey.com>
2019-11-16 16:12:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,6 @@ def nyc_pigeon_organizer(data)
   data.each do |categories, values|
     values.each do |category, names|
       names.each do |name|
-        p names
-    p name
-    p new_hash
       if new_hash.contain?(name)
         new_hash[name] = {}
       end 
