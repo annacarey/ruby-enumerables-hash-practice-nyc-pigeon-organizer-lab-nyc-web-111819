7c9e363769d668f6d68561ba086632e72bc56786
Anna Carey <anna@annajcarey.com>
2019-11-16 17:07:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |categories, values|
+    values.each do |category, names|
+      names.each do |name|
+      if new_hash.key?(name) == false
+        new_hash[name] = {}
+      end
+      if new_hash[name].key?(categories) == false
+        new_hash[name][categories] = []
+      end
+      new_hash[name][categories] << category.to_s
+    end
+    
 end
+end 
+pp new_hash
+end 
+
+#if new_hash[name].key?(category)
+    ##  else 
+    #    new_hash[name][categories] << category
+    #  end 
+    # new_hash[name][categories] = category
+    #if new_hash[name][categories].contain?(category) == false
+    #    new_hash[name][categories].push(category)
+    #  end
